6924c19fcf51613e961ee78f4404dfbd44e4c827
Franco Lepratti Gilles <francolepratti@gmail.com>
2023-07-28 02:33:40 -0300

    update

---
+++ js/transicionPage.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function() {
+    var content = document.getElementById('content');
+    content.style.transform = 'scale(1)'; // Aplicar el zoom (escala) para mostrar la página 2
+  });
